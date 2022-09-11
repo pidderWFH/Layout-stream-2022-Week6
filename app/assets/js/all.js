c3d29ff2b181c07cadac666fef3ab47dbc1e677e
Peter <guanwei0702@gmail.com>
2022-09-11 13:34:48 +0800

    add: index content RWD finished

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,61 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 8,
+  grabCursor: true,
+  loop: true,
+  // effect: 'fade',
+  // fadeEffect: {
+  //   crossFade: true
+  // },
+  autoplay: {
+    delay: 1000,
+  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  breakpoints: {
+    // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 5
+    // },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 5,
+      spaceBetween: 10
+    },
+    // when window width is >= 1360px
+    1360: {
+      slidesPerView: 10,
+      spaceBetween: 16
+    }
+  }
+});
+var swiper = new Swiper(".feedback-Swiper", {
+  // Optional parameters
+  autoHeight: true,
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
